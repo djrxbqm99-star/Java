1d71e1ef0c93c92e6eee83f823771a41f692408e
djrxbqm99 <djrxbqm00@gmail.com>
2026-05-01 15:05:26 +0900

    CodingTest study

---
+++ CodingTest/src/sub1/Solution.java
@@ -0,0 +1,24 @@
+package sub1;
+
+import java.util.Scanner;
+
+/**
+ * 날짜 : 2026.05.01
+ * 이름 : 김민찬
+ * 내용 : 프로그래머스 기출문제 1번 문자출력
+ * 
+ */
+public class Solution {
+    public static void main(String[] args) {
+
+    	Scanner sc = new Scanner(System.in);
+    	
+    	int year; 
+    	
+    	
+    	
+    	System.out.println("출력 : ");
+    	year = sc.nextInt();
+       
+    }
+}
