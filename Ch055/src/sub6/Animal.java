c87da5650753c3764b154b278d8d7bd343c71fc2
djrxbqm99 <djrxbqm00@gmail.com>
2026-05-06 09:53:01 +0900

    자바 다형성 실습

---
+++ Ch055/src/sub6/Animal.java
@@ -0,0 +1,15 @@
+package sub6;
+
+public class Animal {
+	public void move() {
+		System.out.println("Animal move...");
+	}
+
+	
+	// 오버리으드를 위한 추상 메서드
+	public void hunt() {}
+	
+	
+	
+	
+}
---
+++ Ch055/src/sub6/Pet.java
@@ -0,0 +1,8 @@
+package sub6;
+
+public class Pet {
+	public void makesound() {
+		System.out.println("Pet");
+	}
+
+}
