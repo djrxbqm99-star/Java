d3b1f0eed8c968c16209b21882bcfc8779083ecb
djrxbqm99 <djrxbqm00@gmail.com>
2026-05-11 12:36:52 +0900

    자바 StringBulider 실습

---
+++ Ch07/src/sub1/Apple.java
@@ -0,0 +1,36 @@
+package sub1;
+
+public class Apple extends Object{// Java의 모든 클래스는 최상위 클래스 Object 상속
+
+		// 싱글톤 객체
+	private static Apple instance = new Apple();
+	public static Apple gerInstance() {
+		return instance;
+	}
+	
+	public Apple() {
+		// TODO Auto-generated constructor stub
+	}
+
+		
+	
+	
+	
+	
+	
+	private String country;
+	private int price;
+	
+	public Apple(String country, int price) {
+		this.country = country;
+		this.price = price;
+	}
+
+	@Override
+	public String toString() {
+		// Object의 toString을 재정의해서 사용, 일방적으로 속성 정보를 출력
+		return "Apple [country=" + country + ", price=" + price + "]";
+	}
+	
+	
+}
