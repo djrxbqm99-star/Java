35e81836d0e83479066acfa397edfcfc6d1d72cb
djrxbqm99 <djrxbqm00@gmail.com>
2026-05-11 11:09:16 +0900

    자바 예외처리 실습

---
+++ Ch06/src/sub3/ThorwTest.java
@@ -0,0 +1,42 @@
+package sub3;
+
+
+
+/**
+ * 날짜 : 2026.05.11
+ * 이름 : 김민찬
+ * 내용 : Java Throw 예외 발생 시키기 실습
+ */
+class MinusException extends Exception{
+	public MinusException(String message) {
+		super(message);
+	}
+}
+
+class Score{
+	public void check(int score) throws MinusException {
+		if(score < 0) {
+			// 예외 발생시키기
+			throw new MinusException("점수는 음수가 될수 없습니다.");			
+		}else {
+			System.out.println("점수값 : " + score);
+		}
+	}
+}	
+	
+
+public class ThorwTest {
+	public static void main(String[] args) {
+		
+		Score score = new Score();
+		
+		try {
+			score.check(-1);
+		} catch (MinusException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		System.out.println("프로그램 정상 종료");
+	}
+}
