7bc3e699836ad10f3f85997a5c8821cc0fdd9e40
djrxbqm99 <djrxbqm00@gmail.com>
2026-05-15 11:38:24 +0900

    Java PreparedStatment 실습

--- Ch12/src/sub3/User1DTO.java
+++ Ch12/src/sub3/User1DTO.java
@@ -1,8 +1,4 @@
 package sub3;
-// DAO : Data Access object
-// DTI : Data Trasfert object
-
-
 
 public class User1DTO {
 
@@ -10,52 +6,44 @@ public class User1DTO {
 	private String name;
 	private String hp;
 	private int age;
-	
-	
+
 	public String getUserid() {
 		return userid;
 	}
-
 
 	public void setUserid(String userid) {
 		this.userid = userid;
 	}
 
-
 	public String getName() {
 		return name;
 	}
-
 
 	public void setName(String name) {
 		this.name = name;
 	}
 
-
 	public String getHp() {
 		return hp;
 	}
-
 
 	public void setHp(String hp) {
 		this.hp = hp;
 	}
 
-
 	public int getAge() {
 		return age;
 	}
-
 
 	public void setAge(int age) {
 		this.age = age;
 	}
 
-
 	@Override
 	public String toString() {
 		return "User1DTO [userid=" + userid + ", name=" + name + ", hp=" + hp + ", age=" + age + "]";
 	}
+	
 	
 	
 }
